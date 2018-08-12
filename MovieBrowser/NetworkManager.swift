5f4f337d2ab5eedbdfc30d85d22973bfdeb56298
your <paul@25sprout.com>
2018-08-13 05:10:40 +0800

    [Feature] add NetworkManager - import Alamofire - import SwifyJson

---
+++ MovieBrowser/NetworkManager.swift
@@ -0,0 +1,104 @@
+//
+//  NetworkManager.swift
+//  MovieBrowser
+//
+//  Created by Paul Hsieh on 2018/8/13.
+//  Copyright © 2018年 PaulHsieh. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+struct APIResponseModel {
+    var response: [String:JSON]?
+    var success: Bool
+    var errorMessage: String?
+    var error: Error?
+}
+
+class NetworkManager {
+    static let sharedManager: SessionManager = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        configuration.timeoutIntervalForRequest = 300
+        configuration.timeoutIntervalForResource = 300
+        
+        let manager = Alamofire.SessionManager(configuration: configuration)
+        return manager
+    }()
+
+    static let apiBaseUrl = "http://api.themoviedb.org/3/"
+    
+    // MARK: - GET class
+    class Get {
+        public enum GetName:String {
+            case list
+        }
+        
+        private static func urlParser(name:GetName, path:String?) -> String {
+            var apiPath = ""
+            switch name {
+            case .list:
+                apiPath = "discover/movie"
+            }
+            
+            return apiPath
+        }
+        
+        static func request(Name name:GetName, Parameter parameter:[String:AnyObject]?, Path path:String?, completionHandler: @escaping (APIResponseModel) -> ()) {
+            let apiUrl = urlParser(name: name, path: path)
+            let url = apiUrl.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+            
+            sharedManager.request(url, method: .get, parameters: parameter, encoding: URLEncoding.default, headers: nil).validate().responseJSON {
+                response in switch response.result {
+                case .success(let json):
+                    guard let responseJson = json as? [String:AnyObject] else {
+                        completionHandler(APIResponseModel(response: nil,
+                                                           success: false,
+                                                           errorMessage: "CANNOT_CONVERT_TO_JSON",
+                                                           error: nil))
+                        return
+                    }
+                    
+                    let responseDic = JSON(responseJson)
+                    if let statusCode = responseDic["status"].int, let jsonValue = responseDic["data"].dictionary {
+                        if case 200..<300 = statusCode {
+                            completionHandler(APIResponseModel(response: jsonValue,
+                                                               success: true,
+                                                               errorMessage: nil,
+                                                               error: nil))
+                        }
+                        else {
+                            completionHandler(APIResponseModel(response: nil,
+                                                               success: false,
+                                                               errorMessage: "COMMON_NETWORK_ERROR",
+                                                               error: nil))
+                        }
+                    }
+                    else {
+                        completionHandler(APIResponseModel(response: nil,
+                                                           success: false,
+                                                           errorMessage: "COMMON_NETWORK_ERROR",
+                                                           error: nil))
+                    }
+                    
+                    
+                case .failure(let error):
+                    completionHandler(APIResponseModel(response: nil,
+                                                       success: false,
+                                                       errorMessage: "COMMON_NETWORK_ERROR",
+                                                       error: error as Error))
+                    
+                } // end of response switch
+                
+            } // end of request
+            
+        }
+        
+    }
+    
+    // ----- end of Get class ----- //
+    
+}
+
